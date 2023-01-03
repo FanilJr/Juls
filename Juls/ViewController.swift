7bc3187e1ea931a16c6194c4569260ad3f1348e0
Fanil_Jr <ethic91@icloud.com>
2023-01-04 01:37:30 +0500

    Initial Commit

---
+++ Juls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Juls
+//
+//  Created by Fanil_Jr on 04.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
