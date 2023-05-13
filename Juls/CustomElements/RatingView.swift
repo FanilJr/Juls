21eecd51a5f0c990932fdab10e6dca8bd42dc3d3
Fanil_Jr <ethic91@icloud.com>
2023-05-13 14:35:52 +0500

    edit memory

---
+++ Juls/CustomElements/RatingView.swift
@@ -0,0 +1,11 @@
+//
+//  AddRatingView.swift
+//  Juls
+//
+//  Created by Fanil_Jr on 23.04.2023.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ Juls/Match/EndMatchUserView.swift
@@ -0,0 +1,8 @@
+//
+//  EndMatchUserView.swift
+//  Juls
+//
+//  Created by Fanil_Jr on 06.05.2023.
+//
+
+import Foundation
